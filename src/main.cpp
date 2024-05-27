332068a8c2a8dc17db120b58e5124ed52c7b6837
Joel <joeljoels93@gmail.com>
2024-05-27 21:35:13 +0200

    Setting up basic drawing functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <SFML/Graphics.hpp>
 
 const int w_width = 640;
-const int w_height = 480;
+const int w_height = 640;
 
 int main()
 {
-    sf::Window window(
+    sf::RenderWindow window(
         sf::VideoMode(w_width, w_height),
         "Pro elite programmer, Marcus 'Elden Lord' Gladh");
+
+    window.setFramerateLimit(5);
+
+    sf::CircleShape circle(200);
+    circle.setPosition(300, 200);
+    circle.setFillColor(sf::Color::Black);
+
+    sf::RectangleShape head(sf::Vector2f(20, 20));
+    bool toggleShape = false;
+
+    int x = 0;
+    int y = 0;
+
     while (window.isOpen())
     {
+        x += 5;
+        y += 5;
+
+        head.setPosition(x, y);
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -20,6 +39,20 @@ int main()
                 window.close();
             }
         }
+
+        toggleShape = !toggleShape;
+
+        window.clear(sf::Color(155,186,90,255));
+
+        //draw everything here...
+        if (toggleShape)
+        {
+            window.draw(circle);
+        }else{
+            window.draw(head);
+        }
+
+        window.display();
     }
 
     return 0;
