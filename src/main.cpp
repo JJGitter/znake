532fc809ad0c70ac866d40cd4aa8ca6fd6922816
Joel <joeljoels93@gmail.com>
2024-05-27 21:06:19 +0200

    Included SFML library and create window object

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 #include <string>
+#include <SFML/Graphics.hpp>
+
+const int w_width = 640;
+const int w_height = 480;
 
 int main()
 {
-    std::cout << "Pro elite programmer, Marcus 'Elden Lord' Gladh" << std::endl;
+    sf::Window window(
+        sf::VideoMode(w_width, w_height),
+        "Pro elite programmer, Marcus 'Elden Lord' Gladh");
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+    }
+
     return 0;
 }
